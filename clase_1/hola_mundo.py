ab1045407184aaff748017508eeaa1f1e7538b9f
OEM Configuration (temporary user) <juniortovar851@gmail.com>
2024-04-06 17:35:04 -0400

    coloque una variable

--- clase_1/hola_mundo.py
+++ clase_1/hola_mundo.py
@@ -1,2 +1,5 @@
 variable = "Estamos buscando rial, a ver si pegamos chamba con odoo!"
-print("Mezclando python y git, para no ser tan gafo. " + variable)
+variable_121 = "minecraft"
+
+print("Mezclando python y git, para no ser tan gafo. " + variable +" "+variable_121)
+
